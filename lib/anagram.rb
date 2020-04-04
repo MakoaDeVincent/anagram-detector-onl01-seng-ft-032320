2e4023db434fc89917a2e5b448babd2f9a51946f
Makoa DeVincent <makoadevincent@gmail.com>
2020-04-04 22:12:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+#given a word and a list of possible annagrams, selects the correct ones
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(list)
+    good = []
+    trash = []
+    list.each do |listed_word|
+      if listed_word.length == word.length
+        good << listed_word
+      else
+        trash << listed_word
+      end
+    end
+    good
+  end
+
+end
